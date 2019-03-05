dc83166792b99c271d47037f783e0289213f15b4
FirYuen <firyuan@gmail.com>
2019-03-05 15:59:45 +0800

    fix a bug&inmprove register api

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,14 @@
 //@login & register
 const express = require("express");
 const router = express.Router();
-
-const User = require('../../models/User')
+const bcrypt = require("bcrypt");
+const User = require('../../models/User');
 
 // @route GET api/users/test
 // @desc 返回的请求的json数据
 // @access public
 router.get("/test",(req,res)=>{
-    res.json({msg:'logging succesed!'})
+    res.json({msg:'logging succesed!'});  
 })
 
 // @route post api/users/register
@@ -19,16 +19,31 @@ router.post("/register",(req,res)=>{
     //console.log(req.body)
 
     User.findOne({email:req.body.email})
-        .then((User)=>{
-            if (User) {
+        .then((user)=>{
+            if (user) {
                 return res.status(400).json({email:"邮箱已经被注册！"})
             }else{
                 const newUser = new User({
-                    ...req.body
+                    name:req.body.name,
+                    email:req.body.email,
+                    
+                    pwd:req.body.pwd
                 })
                 console.log(newUser)
-                res.send('ok')
+            
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.pwd, salt, function(err, hash) {
+                       if(err) throw err;
+                       newUser.pwd = hash;
+                       
+                       newUser.save()
+                       .then(user=>res.json(user))
+                       .catch(err=>console.log(err)
+                       )
+                    });
+                });
             }
+            
         })
     
 })
